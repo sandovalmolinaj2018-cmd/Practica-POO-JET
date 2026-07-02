1c7ce54b589ce4a2ca73c4c666f8bb224e71f19c
sandovalmolinaj2018-cmd <sandoval.molinaj2018@gmail.com>
2026-07-02 18:23:59 -0300

    MVC-Main

---
+++ main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        // Crear la vista del menú principal
+        VistaMenu vistaMenu = new VistaMenu();
+        vistaMenu.setVisible(true);
+    }
+}
