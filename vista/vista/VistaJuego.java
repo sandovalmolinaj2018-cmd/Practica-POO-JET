7219b813950925c98200a6d21b7978da07b76b48
sandovalmolinaj2018-cmd <sandoval.molinaj2018@gmail.com>
2026-07-02 22:22:15 -0300

    ActualizarVistas

--- vista/vista/VistaJuego.java
+++ vista/vista/VistaJuego.java
@@ -1,10 +1,11 @@
+package vista;
 import javax.swing.JFrame;
 
 // Pantalla del tablero de juego
 public class VistaJuego extends JFrame {
     public VistaJuego() {
         setTitle("Tablero Táctico");
-        setSize(1024, 768);
+        setSize(1920, 1080);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         // Aquí luego pintaremos la cuadrícula (las casillas) usando JPanel
--- vista/vista/VistaMenu.java
+++ vista/vista/VistaMenu.java
@@ -6,12 +6,25 @@ import javax.swing.JButton;
 // Pantalla de inicio
 public class VistaMenu extends JFrame {
     public VistaMenu() {
-        setTitle("Menú Principal - Mortal Tower");
+        setTitle("Menú Principal - Juego de Estrategia");
         setSize(1024, 768);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
+        setLayout(null);
+    
         JButton btnJugar = new JButton("Iniciar Juego");
+        btnJugar.setBounds(20, 200, 180, 40);
+        btnJugar.addActionListener(e -> {
+            VistaJuego vistaJuego = new VistaJuego();
+            vistaJuego.setLocationRelativeTo(null);
+            vistaJuego.setVisible(true);
+            this.dispose(); // Cerramos el menú al iniciar el juego
+        });
+        JButton btnSalir = new JButton("Salir");
+        btnSalir.setBounds(20, 500, 180, 40);
+        btnSalir.addActionListener(e -> System.exit(0));
+
+        add(btnSalir);
         add(btnJugar);
-        // Aquí luego agregaremos los ActionListeners para conectar con el Controlador
     }
 }
